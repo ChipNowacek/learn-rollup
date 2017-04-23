b049234bb20813abf0a8ba0d8a844ab079f8dfd6
ChipNowacek <chip@twostewards.com>
2017-04-23 00:15:37 -0400

    able to reproduce "is not exported" error

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -34,3 +34,6 @@ const printTarget = document.getElementsByClassName('debug__output')[0];
 
 printTarget.innerText = `sayHelloTo('Jason') => ${result1}\n\n`;
 printTarget.innerText += `addArray([1, 2, 3, 4]) => ${result2}`;
+
+import {request} from 'd3-request';
+var req = request('http://ibm.com');
